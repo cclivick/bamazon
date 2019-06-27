e2a0e427ef4eb51f846855068f09373d29f372f4
cclivick <46388110+cclivick@users.noreply.github.com>
2019-06-27 12:05:34 -0500

    Added bamazonManager node app

---
+++ bamazonCustomer.js
@@ -0,0 +1,65 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var colors = require("colors");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password123",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  viewProducts();
+  //connection.end();
+});
+
+function viewProducts() {
+    connection.query("SELECT * FROM products", function (err, result) {
+    if (err) throw err;
+    console.table(result);
+    inquirer
+        .prompt([
+            {
+                type: "number",
+                name:"selectID", 
+                message: "Type the item_id number of the product you wish to purchase:"
+            },
+            {
+                type: "number",
+                name: "howMany",
+                message: "How many would you like to purchase?"
+            }
+        ]).then(answers => {
+          var item = answers.selectID;
+          var quantity = answers.howMany;
+          connection.query("SELECT * FROM products WHERE item_id = " + item + " AND stock_quantity >= " + quantity, function (err, result) {
+            console.log(result);
+            if(!result) {
+              console.log("Insufficient Quantity! Please Try Again!".bgRed.black);
+              connection.end();
+              viewProducts();
+            } 
+          // else {
+          //     inquirer
+          //       .prompt([
+          //         {
+          //           type: "confirm",
+          //           name: "confirm",
+          //           message: "Are you sure you'd like to purchase " + answers.howMany + " of this item?"
+          //         }
+          //       ]);
+          // }
+        });
+      })
+    })
+}
